b6cf4645f392cec934187e87b3b1158f1d60b4d1
Entasa <enc283@nyu.edu>
2021-04-14 18:06:54 -0400

    Fortune

---
+++ Wekk10/FortuneJS.js
@@ -0,0 +1,15 @@
+var answers = ["Yes", "No", "Certainly", "Maybe", "Of Course", "Never", "Definitely","Unsure"]
+var fortune = ["Love awaits you this month. Accept it with open arms and let it transform you.","You need to take control. Hold tight to what is yours and don't let yourself be swayed by some temporary obsession.",
+"Persistence can change failure into extraordinary achievement.", " Take charge of your narrative. Hold true to your words. Remember that you know yourself best!","Let nature take its course, but understand that you are the guiding hand and wherever this water goes...it takes you." ]
+
+var tell_fortune = function() {
+    var response = answers[Math.floor(Math.random()* answers.length)];
+
+    document.getElementById('response').innerHTML = response;
+}
+
+var future = function() {
+    var response = fortune[Math.floor(Math.random()* fortune.length)];
+    document.getElementById('response').innerHTML = response
+
+}
