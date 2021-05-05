9859d0b0be3473e7ca3be4af86176f7d942c83dc
Entasa <enc283@nyu.edu>
2021-05-05 17:28:52 -0400

    final

---
+++ Final/practice.js
@@ -0,0 +1,11 @@
+
+  function closeButton() {
+    var x = document.getElementById('event-maker');
+    x.style.display = "none";
+
+};
+  function openEvent() {
+    var show = document.getElementById('event-maker');
+    show.style.display = "block";
+  }
+ 
