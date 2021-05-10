1ad30f706edacfa8c767efce217dd75303ea02ba
Entasa <enc283@nyu.edu>
2021-05-10 18:12:56 -0400

    update final

---
+++ Final/Diary.js
@@ -0,0 +1,13 @@
+console.log("here!")
+var theUser = document.getElementById("userName").textContent;
+var input = document.getElementById("userinput").value;
+
+var newText;
+function Name() {
+    console.log("here now");
+    newText = userinput.value + theUser;
+   document.querySelector("#userName").innerHTML = newText;
+    // var newText = theUser + input;
+   
+
+};
